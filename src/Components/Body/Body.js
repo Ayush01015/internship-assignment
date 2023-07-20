2de85a2107789cbfeec54078e6414e43005d9432
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-07-21 03:48:48 +0530

    margin fixed

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -50,7 +50,7 @@ const Body = () => {
           <button className="profile-btn">I want to...</button>
         </div>
         <div className="transactions-container">
-          <div className="sub-section">
+          <div className="sub-section sub-section-mt">
             <p className="recent-heading">Recent Transactions</p>
             <div>
               <p className="text-sm">2020-08-07</p>
@@ -58,14 +58,14 @@ const Body = () => {
               <hr className="hr" />
             </div>
           </div>
-          <div className="sub-section">
+          <div className="sub-section sub-section-mt">
             <div>
               <p className="text-sm">2020-08-07</p>
               <p className="text-sm-black">Withdeawl Transfer to Bank-XXX11</p>
               <hr className="hr" />
             </div>
           </div>
-          <div className="sub-section">
+          <div className="sub-section sub-section-mt">
             <div>
               <p className="text-sm">2020-08-07</p>
               <p className="text-sm-black">Withdeawl Transfer to Bank-XXX11</p>
@@ -80,21 +80,23 @@ const Body = () => {
           <h3>Starting Year 2056</h3>
         </div>
         <div className="sub-section  goals-container">
-          <div className="goals">
-            <p className="tx-large">$300,000</p>
-            <span className="text-sm">My Goals</span>
-            <hr className="hr2 hr-blue" />
-          </div>
-          <div className="sub-goal">
+          <div className="goal-flex">
             <div className="goals">
-              <p className="md-2xl">59%</p>
-              <p className="text-sm">Goal Achieved</p>
+              <p className="tx-large">$300,000</p>
+              <span className="text-sm gl">My Goals</span>
               <hr className="hr2 hr-blue" />
             </div>
-            <div className="goals">
-              <p className="md-2xl">K 300</p>
-              <p className="text-sm">Est. Mothly Income</p>
-              <hr className="hr2 hr-blue" />
+            <div className="sub-goal">
+              <div className="goals">
+                <p className="md-2xl">59%</p>
+                <p className="text-sm">Goal Achieved</p>
+                <hr className="hr2 hr-blue" />
+              </div>
+              <div className="goals">
+                <p className="md-2xl">K 300</p>
+                <p className="text-sm">Est. Mothly Income</p>
+                <hr className="hr2 hr-blue" />
+              </div>
             </div>
           </div>
         </div>
@@ -103,31 +105,31 @@ const Body = () => {
           <div className="sub-cotribution-container">
             {/* //margin top */}
             <div className="mt-1 contro">
-              <div className="style-hr"></div>
+              <div className="style-hr style-hr-1"></div>
               <div className="contro-flex">
                 <p className="text-sm">Employeer</p>
                 <span className="md-xl">$73,500</span>
               </div>
             </div>
             <div className="mt-1 contro">
-            <div className="style-hr"></div>
-            <div  className="contro-flex">
+              <div className="style-hr style-hr-2"></div>
+              <div className="contro-flex">
                 <p className="text-sm">Employee</p>
                 <span className="md-xl">$52,500</span>
               </div>
             </div>
             <div className="mt-1 contro">
-                <div className="style-hr"></div>
-                <div  className="contro-flex">
-                    <p className="text-sm">Total Interest</p>
-                    <span className="md-xl">$244,313</span>
-                </div>
-                </div>
+              <div className="style-hr style-hr-3"></div>
+              <div className="contro-flex">
+                <p className="text-sm">Total Interest</p>
+                <span className="md-xl">$244,313</span>
+              </div>
+            </div>
           </div>
         </div>
         <div className="sub-section graph">
-        {/* remove graph class from here and start making Graph component */}
-          <Graph/>
+          {/* remove graph class from here and start making Graph component */}
+          <Graph />
         </div>
         <div className="sub-section">
           <p className="md-xl">How do I Compare to my peers?</p>
@@ -137,111 +139,113 @@ const Body = () => {
 
           <div className="selection">
             <div className="selection-con">
-            <div className="choose">
-              <label htmlFor="age">Age:</label>
-              <select className="options-1" name="age" id="age">
-                <option value="Under30">Under 30</option>
-                <option value="Under20">Under 20</option>
-                <option value="Under50">Under 50</option>
-              </select>
-              <hr className="hr-2" />
+              <div className="choose">
+                <label htmlFor="age">Age:</label>
+                <select className="options-1" name="age" id="age">
+                  <option value="Under30">Under 30</option>
+                  <option value="Under20">Under 20</option>
+                  <option value="Under50">Under 50</option>
+                </select>
+                <hr className="hr-2" />
+              </div>
+              <div className="choose">
+                <label htmlFor="salary">Salary:</label>
+                <select className="options-2" name="age" id="age">
+                  <option value="k20">K 20 - K30</option>
+                  <option value="k10">k 10 - K20</option>
+                </select>
+                <hr className="hr-2" />
+              </div>
+              <div className="choose">
+                <label htmlFor="gender">Gender:</label>
+                <select className="options-3" name="gender" id="age">
+                  <option value="M">Male</option>
+                  <option value="F">Female</option>
+                </select>
+                <hr className="hr-2" />
+              </div>
             </div>
-            <div className="choose">
-              <label htmlFor="salary">Salary:</label>
-              <select className="options-2" name="age" id="age">
-                <option value="k20">K 20 - K30</option>
-                <option value="k10">k 10 - K20</option>
-              </select>
-              <hr className="hr-2" />
-            </div>
-            <div className="choose">
-              <label htmlFor="gender">Gender:</label>
-              <select className="options-3" name="gender" id="age">
-                <option value="M">Male</option>
-                <option value="F">Female</option>
-              </select>
-              <hr className="hr-2" />
-            </div>
-            </div>
-            <ProgressBar/>
+            <ProgressBar />
           </div>
-
         </div>
       </div>
       <div className="container">
-      <div className="section section-3">
-        <span className="heading">Retirement Strategy</span>
-        <div className="slider">
-          <p className="text-md">Employee Contribution</p>
-          <div className="custom-slider">
-            <Slider
-              sx={{
-                width: "100%",
-                [theme.breakpoints.up("xs")]: {
-                  width: "75%",
-                },
-              }}
-              disabled={false}
-              marks={false}
-              max={10}
-              min={0}
-              value={sliderValue}
-              onChange={handleSliderChange}
-              valueLabelDisplay="off"
-            />
-            <div className="slider-data">
-              <span className="span-percentage">{sliderValue} </span>
-              <span className="span-percentage-2">%</span>
+        <div className="section section-3">
+          <span className="heading">Retirement Strategy</span>
+          <div className="slider">
+            <p className="text-md">Employee Contribution</p>
+            <div className="custom-slider">
+              <Slider
+                sx={{
+                  width: "100%",
+                  [theme.breakpoints.up("xs")]: {
+                    width: "75%",
+                  },
+                }}
+                disabled={false}
+                marks={false}
+                max={10}
+                min={0}
+                value={sliderValue}
+                onChange={handleSliderChange}
+                valueLabelDisplay="off"
+              />
+              <div className="slider-data">
+                <span className="span-percentage">{sliderValue} </span>
+                <span className="span-percentage-2">%</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="slider">
-          <p className="text-md">Retirement Age</p>
-          <div className="custom-slider">
-            <Slider
-              sx={{
-                width: "100%",
-                [theme.breakpoints.up("xs")]: {
-                  width: "75%",
-                },
-              }}
-              disabled={false}
-              marks={false}
-              max={10}
-              min={0}
-              value={sliderValueAge}
-              onChange={handleSliderChangeAge}
-              valueLabelDisplay="off"
-            />
-            <div className="slider-data">
-              <span className="span-percentage">{sliderValueAge} </span>
-              <span className="span-percentage-2">%</span>
+          <div className="slider">
+            <p className="text-md">Retirement Age</p>
+            <div className="custom-slider">
+              <Slider
+                sx={{
+                  width: "100%",
+                  [theme.breakpoints.up("xs")]: {
+                    width: "75%",
+                  },
+                }}
+                disabled={false}
+                marks={false}
+                max={10}
+                min={0}
+                value={sliderValueAge}
+                onChange={handleSliderChangeAge}
+                valueLabelDisplay="off"
+              />
+              <div className="slider-data">
+                <span className="span-percentage">{sliderValueAge} </span>
+                <span className="span-percentage-2">%</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="slider-data-2">
-          <p className="text-md">Employeer Contribution</p>
-          <p className="text-md-500">8.4%</p>
-        </div>
-        <div className="slider-data-2">
-          <p className="text-md">Intrest Rate</p>
-          <p className="text-md-500">5%</p>
-        </div>
-        <div className="footer-section-3">
+          <div className="slider-data-2">
+            <p className="text-md">Employeer Contribution</p>
+            <p className="text-md-500">8.4%</p>
+          </div>
+          <div className="slider-data-2">
+            <p className="text-md">Intrest Rate</p>
+            <p className="text-md-500">5%</p>
+          </div>
+          <div className="footer-section-3">
             <button className="update-btn">Update</button>
             <span className="help-docs">View Help Docs &#62;</span>
+          </div>
         </div>
-      </div>
-      <div className="extras">
-            <div>
-                <div className="vertical-line"></div>
-            </div>
-            <div className="extra-width">
-                <p>Are you considering a having <span className="bold">Housing Advance?</span> </p>
-                <p className="text-sm">Limited time reduced interest</p>
-                <p className="learn-more">learn More &#62;</p>
-            </div>
-      </div>
+        <div className="extras">
+          <div>
+            <div className="vertical-line"></div>
+          </div>
+          <div className="extra-width">
+            <p>
+              Are you considering a having{" "}
+              <span className="bold">Housing Advance?</span>{" "}
+            </p>
+            <p className="text-sm">Limited time reduced interest</p>
+            <p className="learn-more">learn More &#62;</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Graph/Graph.js
+++ src/Components/Graph/Graph.js
@@ -1,33 +1,50 @@
-import * as React from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "@mui/x-charts";
+import React from 'react';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryStack } from 'victory';
+/**
+    Still working in Graph (pending)
+ */
+import GraphIMG from "../../assets/img/graph"
+import "./Graph.css"
+const exponentialData = (startValue, factor, count) => {
+  const data = [];
+  let currentValue = startValue;
+  for (let i = 0; i < count; i++) {
+    data.push({ x: `Group ${i + 1}`, y: currentValue });
+    currentValue *= factor;
+  }
+  return data;
+};
 
-import "./Graph.css";
-const data = [
-    { data: [10, 12, 15, 20, 25], stack: 'A', label: 'series A1' },
-    { data: [20, 24, 30, 56, 62], stack: 'A', label: 'series A1' },
-    { data: [60, 65, 70, 105, 110], stack: 'A', label: 'series A2' },
-  ];
-  
 const Graph = () => {
+  const data = exponentialData(1, 2, 5); // Start with 1, multiply by 2, and get 5 data points
+
+  // Define the colors for the gradient
+  const colors = ['#00FF00', '#FFFF00', '#FF0000'];
+
+  // Create a function to calculate the gradient color based on the y-value
+  const getGradientColor = (datum) => {
+    const yValue = datum.y;
+    const max = Math.max(...data.map((d) => d.y));
+    const min = Math.min(...data.map((d) => d.y));
+    const index = Math.floor(((yValue - min) / (max - min)) * (colors.length - 1));
+    return colors[index];
+  };
+
   return (
-    <div 
-    style={{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-    }}
-    >
-      <p>GRAPH</p>
+    <div className='graph-plot'>
+    <img src={GraphIMG} alt=''/>
+    {/* <VictoryChart domainPadding={20}>
+      <VictoryAxis tickFormat={(t) => `Group ${t}`} />
+      <VictoryAxis dependentAxis />
+      <VictoryStack colorScale={colors}>
+        {data.map((entry, index) => (
+          <VictoryBar key={index} data={[entry]} x="x" y="y" />
+        ))}
+      </VictoryStack>
+    </VictoryChart> */}
     </div>
   );
 };
 
 export default Graph;
+
